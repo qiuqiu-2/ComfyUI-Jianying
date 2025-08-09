9b1af2b199da34c7bbe1280ed320615b060ad2f0
qiuqiu-2 <1130203386@qq.com>
2025-08-09 23:04:18 +0800

    feat: 添加剪映草稿图片添加器节点
    新增JianyingDraftImageAdder节点类，用于支持向剪映草稿添加图片的功能

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
-from .nodes import JianyingDraftNode, JianyingDraftAudioAdder
+from .nodes import JianyingDraftNode, JianyingDraftAudioAdder, JianyingDraftImageAdder
 
 NODE_CLASS_MAPPINGS = {
     "JianyingDraftNode": JianyingDraftNode,
     "JianyingDraftAudioAdder": JianyingDraftAudioAdder,
+    "JianyingDraftImageAdder": JianyingDraftImageAdder,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "JianyingDraftNode": "剪映草稿创建器",
     "JianyingDraftAudioAdder": "剪映草稿音频添加器",
+    "JianyingDraftImageAdder": "剪映草稿图片添加器",
 }
